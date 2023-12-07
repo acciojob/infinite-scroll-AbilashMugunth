9a159ace732ead3619f11b71b0f108f01d66d50b
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-07 15:47:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here!
+let list = document.querySelector("#infi-list");
+let count = 0;
 
+for (let i = 0; i < 7 ; i++) {
+  let listItem = document.createElement("li");
+  listItem.innerText = `Item ${++count}`;
+  list.appendChild(listItem);
+}
+
+list.addEventListener("scroll", () => {
+  let listItem = document.createElement("li");
+  listItem.innerText = `Item ${++count}`;
+  list.appendChild(listItem);
+});
